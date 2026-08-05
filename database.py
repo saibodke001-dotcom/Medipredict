be86b3db9f79b12dbdf0ee957b301950e9b0165d
saibodke001-dotcom <saibodke001@gmail.com>
2026-08-05 14:01:04 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./medical.db"
+# 🚀 VERCEL FIX: Vercel has a read-only filesystem. We must write the database to /tmp if deployed there!
+if os.environ.get("VERCEL") == "1":
+    SQLALCHEMY_DATABASE_URL = "sqlite:////tmp/medical.db"
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./medical.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
